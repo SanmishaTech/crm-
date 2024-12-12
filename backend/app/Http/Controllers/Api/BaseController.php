0d920bdbe45b37c468bedf14bd6caec6e729af29
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-12-12 12:01:00 +0530

    added baseCOntroller

--- backend/app/Http/Controllers/Api/BaseController.php
+++ backend/app/Http/Controllers/Api/BaseController.php
@@ -8,14 +8,14 @@ use App\Http\Controllers\Controller as Controller;
 class BaseController extends Controller
 {
     /**
-     * success response method.
+     * status response method.
      *
      * @return \Illuminate\Http\Response
      */
     public function sendResponse($result, $message)
     {
         $response = [
-            'success' => true,
+            'status' => true,
             'message' => $message,
             'data'    => $result,
         ];
@@ -31,12 +31,12 @@ class BaseController extends Controller
     public function sendError($error, $errorMessages = [], $code = 401)
     {
         $response = [
-            'success' => false,
+            'status' => false,
             'message' => $error,
         ];
   
         if(!empty($errorMessages)){
-            $response['data'] = $errorMessages;
+            $response['errors'] = $errorMessages;
         }
   
         return response()->json($response, $code);
