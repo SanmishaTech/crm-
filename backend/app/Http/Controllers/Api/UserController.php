05fa15aeab79f0aac9a7c9a789389c7993a1997d
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-12-12 12:03:47 +0530

    changes in UserController

--- backend/app/Http/Controllers/Api/UserController.php
+++ backend/app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use Validator;
 use App\Models\User;
-use App\Models\Profile;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
@@ -13,12 +12,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Resources\ProfileResource;
+use App\Http\Resources\EmployeeResource;
 use App\Http\Controllers\Api\BaseController;
 
 class UserController extends BaseController
 {
-
 
      /**
      * Login User
@@ -38,7 +36,7 @@ class UserController extends BaseController
             $user = Auth::user();
             $token =  $user->createToken($user->name)->plainTextToken;
             $profile = Employee::where('user_id', $user->id)->first();
-            return $this->sendResponse(['user'=>new UserResource($user), 'profile'=>new ProfileResource($profile), 'token'=>$token], 'User login successfully.');
+            return $this->sendResponse(['user'=>new UserResource($user), 'profile'=>new EmployeeResource($profile), 'token'=>$token], 'User login successfully.');
 
         } else{
             return $this->sendError('Invalid Credentials.', ['error'=>'Invalid Credentials']);
@@ -54,103 +52,4 @@ class UserController extends BaseController
         return $this->sendResponse([], 'User logged out successfully.');
     }
 
-
-    /**
-     * All User
-     */
-    public function index(): JsonResponse
-    {
-        $users = User::all();
-        
-        return $this->sendResponse(['Users'=> UserResource::collection($users)], "all users retrived sucessfully");
-    }
-
-    /**
-     * Store User
-     */
-    public function store(Request $request){
-        
-       $user = new User();
-       $user->name = $request->input('name');
-       $user->email = $request->input('email');
-       $user->password = Hash::make($request->input('password'));
-       $user->save();
-       
-        $memberRole = Role::where("name", 'member')->first();
-      
-       $user->assignRole($memberRole);
-       
-       $profile = new Profile();
-       $profile->user_id = $user->id;
-       $profile->name = $user->name;
-       $profile->email = $user->email;
-       $profile->save();
-      
-       return $this->sendResponse(['User'=> new UserResource($user), 'profile'=>new ProfileResource($profile)], "user stored successfully");
-
-    }
-
-    /**
-     * Update User
-     */
-    public function update(Request $request, string $id): JsonResponse
-    {
-        $user = User::find($id);
-        if(!$user){
-            return $this->sendError("User not found", ['error'=>'User not found']);
-        }
-       $user->name = $request->input('name');
-       $user->email = $request->input('email');
-       $user->password = Hash::make($request->input('password'));
-       $user->save();
-    
-        $memberRole = Role::where("name", 'member')->first();
-    
-       $user->assignRole($memberRole);
-       
-       $profile = Profile::where('user_id',$user->id)->first();
-       $profile->name = $user->name;
-       $profile->email = $user->email;
-       $profile->save();
-      
-       return $this->sendResponse(['User'=> new UserResource($user), 'profile'=>new ProfileResource($profile)], "User Updated successfully");
-
-    }
-
-    /**
-     * delete User
-     */
-    public function destroy(string $id): JsonResponse
-    {
-        $user = User::find($id);
-
-        if(!$user){
-            return $this->sendError("User not found", ['error'=>'User not found']);
-        }
-        $profile = Profile::where('user_id', $user->id)->first();
-        $profile->delete();
-        $user->delete();
-
-        return $this->sendResponse([], "User and his Profile deleted successfully");
-
-    }
-
-    
-    /**
-     * show User
-     */
-    public function show(string $id): JsonResponse
-    {
-        $user = User::find($id);
-
-        if(!$user){
-            return $this->sendError("User not found", ['error'=>'User not found']);
-        }
-        // $profile = Profile::where('user_id', $user->id)->first();
-        // $profile->delete();
-        // $user->delete();
-
-        return $this->sendResponse(['User'=> new UserResource($user)], "User Retrieved successfully");
-
-    }
 }
