5b8681ba21087f9d30216be7e1f9ee2983026a0a
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2024-12-26 15:23:32 +0530

    quotation api

--- backend/app/Models/LeadProduct.php
+++ backend/app/Models/LeadProduct.php
@@ -8,11 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class LeadProduct extends Model
 {
     protected $fillable = [
-        'lead_id', 'product_id', 'quantity',
+        'lead_id', 'product_id', 'quantity','rate','gst_amount','amount_without_gst','total_amount'
     ];
 
     public function lead()
     {
         return $this->belongsTo(Lead::class);
     }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    
 }
