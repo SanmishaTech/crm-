9b251e4f560d874b5ef59cf867e4c261b7f5bdc6
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-01-07 18:24:18 +0530

    gfr

--- frontend/src/Components/Leads/Sidebar.tsx
+++ frontend/src/Components/Leads/Sidebar.tsx
@@ -204,7 +204,7 @@ export default function Sidebar({ className, onFilterChange }: SidebarProps) {
           </Popover>
         </div>
         {/* Product Filter */}
-        <div className="mt-2">
+        {/* <div className="mt-2">
           <Popover open={openProductFilter} onOpenChange={setOpenProductFilter}>
             <PopoverTrigger asChild>
               <Button
@@ -213,7 +213,6 @@ export default function Sidebar({ className, onFilterChange }: SidebarProps) {
                 aria-expanded={openProductFilter ? "true" : "false"}
                 className="w-[200px] justify-between"
               >
-                {/* Check if productIds exists, then find the label in productOptions */}
                 {productIds
                   ? productOptions?.find(
                       (product) => product?.value === productIds
@@ -269,7 +268,7 @@ export default function Sidebar({ className, onFilterChange }: SidebarProps) {
               </Command>
             </PopoverContent>
           </Popover>
-        </div>
+        </div> */}
 
         {/* Reset Filters Button */}
         <div className="mt-4">
