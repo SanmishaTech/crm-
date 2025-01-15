25781a4c5897674139afa52f6c2a1f5820383b88
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-01-15 23:34:47 +0530

    phase-2

--- backend/resources/views/quotation/quotation.blade.php
+++ backend/resources/views/quotation/quotation.blade.php
@@ -50,10 +50,7 @@
             border: none;
             padding: 0;
         }
-        .invoice-box .information td {
-            border: none;
-            padding: 0;
-        }
+        
         .invoice-box .totals td {
             border: none;
             text-align: right;
@@ -104,7 +101,8 @@
     <div class="logo">
         <img src="{{ public_path('images/Renuka.jpg') }}" alt="Renuka Enterprises Logo">
     </div>
-    <h4 style="text-align: center">QUOTATION</h4>
+    <h4 style="text-align: center">                    version: {{@$leads->quotation_version}}<br>
+    </h4>
     <table style="width: 100%; margin: 10px 0;">
         <tr>
             <td style="text-align: left;"><strong>Quotation Ref No: {{@$leads->quotation_number}}</strong></td>
@@ -121,8 +119,8 @@
         
 
         <div>
-            <p>We are channel partners with following manufacturing companies/OEM.</p>
-            <table style="width: 100%; margin-top: 15px; border-collapse: collapse;">
+            <p style="text-align: center;">We are channel partners with following manufacturing companies/OEM.</p>
+            <table style="width: 100%; margin-top: 15px; border-collapse: collapse; margin-left: auto; margin-right: auto;">
                 <tr>
                     <td style="padding: 8px; border: 1px solid black; width: 50%;"><strong>FLIR Systems India - USA</strong></td>
                     <td style="padding: 8px; border: 1px solid black;">Thermal Imaging Cameras</td>
@@ -144,7 +142,7 @@
                     <td style="padding: 8px; border: 1px solid black;">Electrical HV & LT Equipments</td>
                 </tr>
             </table>
-            <p style="margin-top: 15px;">If you have any queries, our engineers are available to assist you on 9870201624/8928056500.</p>
+            <p style="text-align: center; margin-top: 15px;">If you have any queries, our engineers are available to assist you on 9870201624/8928056500.</p>
         </div>
     </div>
 
@@ -156,25 +154,7 @@
     
     <div class="invoice-box">
         <table cellpadding="0" cellspacing="0">
-            <tr class="information">
-                <td colspan="4" style="padding-left:10px; ">
-                  <strong>CRM</strong><br>
-                     <P>Maharashtra - 400605</P>
-                </td>
-                <td colspan="4" class="text-end" style=" text-align: right; padding-right:10px; padding-top:10px;">
-                    
-                   
-                    version: {{@$leads->quotation_version}}<br>
-                    <strong>To</strong><br>
-                    test user<br>
-                    thane,
-                    Maharashtra<br> 
-                    Mobile: 9999887766 <br>
-                    Email: test@gmail.com
-                    <br><br><br>
-                </td>
-            </tr>
-            
+ .            
             <tr class="heading">
                 <th>Sl.</th>
                 <th>Product Name</th>
@@ -231,27 +211,113 @@
             </tr>
         </table>
 
-        <div class="text-center" style="margin-top: 20px;">
+        <!-- <div class="text-center" style="margin-top: 20px;">
             <p>GST% 18% &nbsp;&nbsp; Taxable ₹{{$leads->total_taxable}} &nbsp;&nbsp; CGST ₹3.60&nbsp;&nbsp; SGST ₹3.60</p>
-        </div>
+        </div> -->
 
         <div class="terms-conditions" style="margin-top: 40px;">
-            <h5>Terms and Conditions:</h5>
-            <ul>
-                <li>Quotation is valid for 15 days from the date of issue.</li>
-                <li>50% advance payment is required with purchase order.</li>
-                <li>Balance payment should be made before delivery of goods.</li>
-                <li>Delivery period: 7-10 working days after confirmation of order.</li>
-                <li>Prices are subject to change without prior notice.</li>
-                <li>GST rates are applicable as per government norms.</li>
-                <li>Warranty as per manufacturer's terms and conditions.</li>
-                <li>Installation and training will be provided if applicable.</li>
+           
+            <p style="font-weight: bold;"><u>Terms and Conditions</u></p>
+            <ul style="list-style-type: disc; padding-left: 20px;">
+                <li>Prices               	:	FOR Mumbai</li>
+                <li>GST                  	:	Extra 18%</li>
+                <li>Freight             	:	Inclusive</li>
+                <li>Packing & Forwarding	:	Inclusive</li>
+                <li>Payment Terms       	:	100% against PI</li>
+                <li>Delivery           	:	4 weeks from the date of PO</li>
             </ul>
+            
+            <div style="text-align: left; margin-top: 30px;">
+                <p style="margin: 0;">For Renuka Enterprises</p>
+                <img src="{{ public_path('images/renuka sign.jpg') }}" alt="Signature" style="max-width: 150px; height: auto;">
+                <p style="margin: 0;">Manish</p>
+            </div>
+
+            <p style="text-align: center; margin-top: 20px; margin-bottom: 10px; font-weight: bold;">Our Product Range</p>
+            
+            <table style="width: 100%; border-collapse: collapse; font-size: 9px;">
+                <tr>
+                    <td style="padding: 2px; border: none;">Air Quality Meters</td>
+                    <td style="padding: 2px; border: none;">Air Flow Meters</td>
+                    <td style="padding: 2px; border: none;">Automotive Meters</td>
+                    <td style="padding: 2px; border: none;">Battery Testers</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Borescope Cameras | Videoscopes</td>
+                    <td style="padding: 2px; border: none;">Cable Tracers</td>
+                    <td style="padding: 2px; border: none;">Calibrators</td>
+                    <td style="padding: 2px; border: none;">Circuit Identifiers</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Clamp Meters</td>
+                    <td style="padding: 2px; border: none;">Continuity Testers</td>
+                    <td style="padding: 2px; border: none;">Decade Boxes</td>
+                    <td style="padding: 2px; border: none;">FLIR Thermal Imaging Cameras</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">FLIR Videoscopes</td>
+                    <td style="padding: 2px; border: none;">FLIR Flex Clamp Meters</td>
+                    <td style="padding: 2px; border: none;">Distance Meters</td>
+                    <td style="padding: 2px; border: none;">Electromagnetic Field Meters</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Force Gauges</td>
+                    <td style="padding: 2px; border: none;">Gas Detectors and Analyzers</td>
+                    <td style="padding: 2px; border: none;">Ground Resistance Testers</td>
+                    <td style="padding: 2px; border: none;">Heat Index Meters</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Humidity Meters| Hygrometers</td>
+                    <td style="padding: 2px; border: none;">LCR Meters</td>
+                    <td style="padding: 2px; border: none;">Light Meters</td>
+                    <td style="padding: 2px; border: none;">Magnetic Field Meters</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Megohm meters | Insulation Testers</td>
+                    <td style="padding: 2px; border: none;">Milliohm | Micro-Ohm Meters</td>
+                    <td style="padding: 2px; border: none;">Moisture Meters</td>
+                    <td style="padding: 2px; border: none;">Multimeters</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Non-contact Voltage Testers and Current Testers</td>
+                    <td style="padding: 2px; border: none;">Oscilloscopes</td>
+                    <td style="padding: 2px; border: none;">Pedometers</td>
+                    <td style="padding: 2px; border: none;">Phase Rotation | Motor Rotation Indicators</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">PID Controllers</td>
+                    <td style="padding: 2px; border: none;">Power Analyzers</td>
+                    <td style="padding: 2px; border: none;">Power Supplies</td>
+                    <td style="padding: 2px; border: none;">Pressure Meters | Manometers</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Receptacle Testers and Analyzers</td>
+                    <td style="padding: 2px; border: none;">Refractometers</td>
+                    <td style="padding: 2px; border: none;">Scales</td>
+                    <td style="padding: 2px; border: none;">Sound Meters | Decibel Meters</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Stopwatches and Timers</td>
+                    <td style="padding: 2px; border: none;">Tachometers</td>
+                    <td style="padding: 2px; border: none;">Thermometers</td>
+                    <td style="padding: 2px; border: none;">Thickness Gauges</td>
+                </tr>
+                <tr>
+                    <td style="padding: 2px; border: none;">Vibration Meters</td>
+                    <td style="padding: 2px; border: none;">Water Quality Meters</td>
+                    <td style="padding: 2px; border: none;">And Many more........</td>
+                    <td style="padding: 2px; border: none;"></td>
+                </tr>
+                <tr>
+                    <td colspan="4" style="text-align: center; padding: 2px; border: none;">
+                        <a href="http://www.extech.com/">www.extech.com</a> &nbsp;&nbsp; 
+                        <a href="https://www.flir.com/">www.flir.com</a>
+                    </td>
+                </tr>
+            </table>
         </div>
 
-        <div class="text-center" style="margin-top: 40px;">
-            <p>This is a computer-generated document. No signature is required.</p>
-        </div>
+       
     </div>
  </body>
 </html>
