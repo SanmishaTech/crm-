e3be7493eefd6447a0a4488561492b3bbdc62e43
Vipulkoli2000 <Vipulkoli2323@gmail.com>
2026-04-16 13:02:03 +0530

    feat: initialize React application with Redux, TanStack Query, and ThemeProvider configurations

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
@@ -28,7 +25,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           </ThemeProvider>
         </React.StrictMode>
       </Provider>
-      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
     </QueryClientProvider>
   </BrowserRouter>
 );
